67d8552d6a70b6ddbd0f7c4d0fc40536ccc91e95
Emiliano Massari <158219820+epsyFRG@users.noreply.github.com>
2025-10-24 16:23:35 +0200

    Initial setup

---
+++ src/main/java/com/eventmanager/EventsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.eventmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsManagerApplication.class, args);
+	}
+
+}
